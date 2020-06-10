6962e02b58540a834fb699ee4e79ce57e9874de7
Andrew Park <acpark123@gmail.com>
2020-06-10 21:07:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   
   planeteer_calls.any? do |string|
-    sting > 4  
+    sting.length > 4  
   end
   
 end
